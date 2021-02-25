c6632d1727a072b70cbe9c52cb94bb4592cb1554
jaiguers <jaiguers@outlook.com>
2021-02-24 23:22:36 -0500

    Corregir configuración

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,15 +6,7 @@ export const environment = {
   production: false,
   urlApi: 'https://platzi-store.herokuapp.com',
   urlMembership: 'http://localhost:9087/membership/',
-  firebase: {
-    apiKey: 'AIzaSyDg41kkE1FHSwl-ym4nJEV9YLCc1IM4CyA',
-    authDomain: 'tienda-d15c7.firebaseapp.com',
-    databaseURL: 'https://tienda-d15c7.firebaseio.com',
-    projectId: 'tienda-d15c7',
-    storageBucket: 'tienda-d15c7.appspot.com',
-    messagingSenderId: '196865843876',
-    appId: '1:196865843876:web:a2164f30d2a55f8b20bc31'
-  }
+
 };
 
 /*
